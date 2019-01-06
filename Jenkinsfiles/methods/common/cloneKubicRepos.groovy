af7ac7c90c03bc5d179a38c5f69f89501d42eeaa
Ludovic Cavajani <lcavajani@suse.com>
2019-01-06 13:14:01 +0100

    create git repos in cloneKubicRepos
    Signed-off-by: Ludovic Cavajani <lcavajani@suse.com>

--- Jenkinsfiles/methods/common/cloneKubicRepos.groovy
+++ Jenkinsfiles/methods/common/cloneKubicRepos.groovy
@@ -2,6 +2,9 @@ def call(Map conf) {
     def kubicRepo = 'https://github.com/kubic-project/'
 
     timeout(5) {
+        sh(script: "mkdir -p ${WORKSPACE}/{automation,salt,velum}")
+        sh(script: "mkdir -p ${WORKSPACE}/{caasp-container-manifests,caasp-services}")
+
         dir("automation") {
             checkout([$class: 'GitSCM', branches: [[name: "*/${conf.branchName}"]],
             userRemoteConfigs: [[url: (kubicRepo + 'automation.git')]], extensions: [[$class: 'CleanCheckout']]])
--- Jenkinsfiles/methods/common/setUpWorkspace.groovy
+++ Jenkinsfiles/methods/common/setUpWorkspace.groovy
@@ -1,7 +1,5 @@
 def call() {
     sh(script: "mkdir -p ${WORKSPACE}/logs")
-    sh(script: "mkdir -p ${WORKSPACE}/{automation,salt,velum}")
-    sh(script: "mkdir -p ${WORKSPACE}/{caasp-container-manifests,caasp-services}")
 }
 
 return this;
