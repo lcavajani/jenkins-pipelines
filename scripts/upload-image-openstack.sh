984b13159378c904d8a88e7d4eeff35ba35a4d83
Ludovic Cavajani <lcavajani@suse.com>
2019-01-18 11:03:52 +0100

    fix shellcheck issues
    Signed-off-by: Ludovic Cavajani <lcavajani@suse.com>

--- scripts/upload-image-openstack.sh
+++ scripts/upload-image-openstack.sh
@@ -6,40 +6,41 @@ OPENRC=$1
 IMAGE_URL=$2
 INSECURE=${3:-""}
 
-source $OPENRC
+# shellcheck source=/dev/null
+source "$OPENRC"
 
-IMAGE_NAME="$(basename $IMAGE_URL)"
-IMAGE_PATH="$(pwd)/$(basename $IMAGE_URL)"
-IMAGE_BUILD=$(echo $IMAGE_NAME | sed -n 's/.*\(Build.*\).qcow2/\1/p')
-IMAGE_VERSION=$(echo $IMAGE_NAME | sed -n 's/.*CaaSP-\(.*\)\.x86_64-15.0-CaaSP-Stack-OpenStack-Cloud.*/\1/p')
+IMAGE_NAME="$(basename "$IMAGE_URL")"
+IMAGE_PATH="$(pwd)/$(basename "$IMAGE_URL")"
+IMAGE_BUILD=$(echo "$IMAGE_NAME" | sed -n 's/.*\(Build.*\).qcow2/\1/p')
+IMAGE_VERSION=$(echo "$IMAGE_NAME" | sed -n 's/.*CaaSP-\(.*\)\.x86_64-15.0-CaaSP-Stack-OpenStack-Cloud.*/\1/p')
 
 echo "INFO: Checking if we already have this image remotely: $IMAGE_NAME"
 
-if ! openstack ${INSECURE:+--insecure} image list --private | grep -q " $IMAGE_NAME "; then
+if ! openstack ${INSECURE:+--insecure} image list --private | grep -q "$IMAGE_NAME"; then
     echo "INFO: Image does not exist remotely"
 
     echo "INFO: Checking if we need to download image locally: $IMAGE_PATH"
-    if [[ ! -f $IMAGE_PATH ]]; then
+    if [[ ! -f "$IMAGE_PATH" ]]; then
         echo "INFO: Image does not exist locally, downloading..."
         echo "INFO: Downloading image: $IMAGE_URL"
-        curl -sSL $IMAGE_URL -o $IMAGE_PATH
+        curl -sSL "$IMAGE_URL" -o "$IMAGE_PATH"
     else
         echo "INFO: Image found locally, skipping..."
     fi
 
     echo "INFO: Uploading image: $IMAGE_NAME"
-    openstack --insecure image create $IMAGE_NAME --private
+    openstack --insecure image create "$IMAGE_NAME" --private
         --disk-format qcow2 --container-format bare
-        --min-disk 40 --file $IMAGE_PATH \
+        --min-disk 40 --file "$IMAGE_PATH" \
         --property caasp-version="$IMAGE_VERSION" \
         --property caasp-build="$IMAGE_BUILD"
 else
     echo "INFO: Image already exists..."
-    openstack ${INSECURE:+--insecure} image show $IMAGE_NAME
+    openstack ${INSECURE:+--insecure} image show "$IMAGE_NAME"
     echo "INFO: Skipping upload..."
 fi
 
-if [[ -f $IMAGE_PATH ]]; then
+if [[ -f "$IMAGE_PATH" ]]; then
     echo "INFO: Cleaning local image"
-    rm -vf $IMAGE_PATH
+    rm -vf "$IMAGE_PATH"
 fi
--- scripts/upload-image-vmware.sh
+++ scripts/upload-image-vmware.sh
@@ -5,35 +5,35 @@ set -euo pipefail
 VAR_FILE=$1
 IMAGE_URL=$2
 
-IMAGE_NAME="$(basename $IMAGE_URL)"
-IMAGE_PATH="$(pwd)/$(basename $IMAGE_URL)"
+IMAGE_NAME="$(basename "$IMAGE_URL")"
+IMAGE_PATH="$(pwd)/$(basename "$IMAGE_URL")"
 
 caasp_vmware() {
-  python3 ./caasp-vmware.py --var-file $VAR_FILE $@
+  python3 ./caasp-vmware.py --var-file "$VAR_FILE" "$@"
 }
 
 echo "INFO: Checking if we already have this image remotely: $IMAGE_NAME"
 
-if ! caasp_vmware listimages | grep -q $IMAGE_NAME; then
+if ! caasp_vmware listimages | grep -q "$IMAGE_NAME"; then
     echo "INFO: Image does not exist remotely"
 
     echo "INFO: Checking if we need to download image locally: $IMAGE_PATH"
-    if [[ ! -f $IMAGE_PATH ]]; then
+    if [[ ! -f "$IMAGE_PATH" ]]; then
         echo "INFO: Image does not exist locally, downloading..."
         echo "INFO: Downloading image: $IMAGE_URL"
-        curl -sSL $IMAGE_URL -o $IMAGE_PATH
+        curl -sSL "$IMAGE_URL" -o "$IMAGE_PATH"
     else
         echo "INFO: Image found locally, skipping..."
     fi
 
     echo "INFO: Uploading image: $IMAGE_NAME"
-    caasp_vmware pushimage --source-media $IMAGE_PATH
+    caasp_vmware pushimage --source-media "$IMAGE_PATH"
 else
     echo "INFO: Image already exists..."
     echo "INFO: Skipping upload..."
 fi
 
-if [[ -f $IMAGE_PATH ]]; then
+if [[ -f "$IMAGE_PATH" ]]; then
     echo "INFO: Cleaning local image"
-    rm -vf $IMAGE_PATH
+    rm -vf "$IMAGE_PATH"
 fi
