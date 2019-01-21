15cd8d5b34b803c4b5c2b86824d86e7856e3f607
Ludovic Cavajani <lcavajani@suse.com>
2019-01-21 11:56:15 +0100

    test user-scoped credentials
    Signed-off-by: Ludovic Cavajani <lcavajani@suse.com>

---
+++ Jenkinsfiles/Jenkinsfile-test-user-scoped.groovy
@@ -0,0 +1,22 @@
+//properties([
+//    parameters([
+//        credentials(name: 'jenkins-api')
+//    ])
+//])
+
+node('qa-caasp') {
+    checkout scm
+
+    def common = load('./Jenkinsfiles/methods/common.groovy')
+    def defaultJobParametersMap = common.readDefaultJobParameters()
+
+    stage('preparation') {
+        stage('node Info') {
+            common.nodeInfo()
+
+//            withCredentials([usernamePassword(credentialsId: '${jenkins-api}', usernameVariable: 'JENKINS_USER', passwordVariable: 'JENKINS_PASSWORD')]) {
+                sh(script: "echo \$JENKINS_USER")
+//            }
+        }
+    }
+}
