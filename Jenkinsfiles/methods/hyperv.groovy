018f2e61b8d7b1e226c84a1d83e50305918b5030
Ludovic Cavajani <lcavajani@suse.com>
2019-01-22 17:47:33 +0100

    remove remaining bracket
    Signed-off-by: Ludovic Cavajani <lcavajani@suse.com>

--- Jenkinsfiles/methods/hyperv.groovy
+++ Jenkinsfiles/methods/hyperv.groovy
@@ -1,6 +1,6 @@
 def pushImage(Map jobParams) {
     timeout(120) {
-        withEnv(["SSHUSER=${jobParams.credentials.hyperv_username}", "SSHPASS=${jobParams.credentials.hyperv_password}")]) {
+        withEnv(["SSHUSER=${jobParams.credentials.hyperv_username}", "SSHPASS=${jobParams.credentials.hyperv_password}"]) {
             sh(script: "set -o pipefail; sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \"${SSHUSER}\"@${jobParams.platformEndpoint} 'Get-ChildItem Env:;git checkout ${BRANCH_NAME}; git pull; caasp-hyperv.ps1 fetchimage -caaspImageSourceUrl ${jobParams.imageSourceUrl} -nochecksum' 2>&1 | tee ${WORKSPACE}/logs/caasp-hyperv.log")
         }
     }
@@ -9,7 +9,7 @@ def pushImage(Map jobParams) {
 def createEnvironment(Map jobParams) {
     timeout(120) {
         // https://github.com/PowerShell/Win32-OpenSSH/issues/1049 -> Use SSH password
-        withEnv(["SSHUSER=${jobParams.credentials.hyperv_username}", "SSHPASS=${jobParams.credentials.hyperv_password}")]) {
+        withEnv(["SSHUSER=${jobParams.credentials.hyperv_username}", "SSHPASS=${jobParams.credentials.hyperv_password}"]) {
             sh(script: "set -o pipefail; sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \"${SSHUSER}\"@${jobParams.platformEndpoint} 'Get-ChildItem Env:; git checkout ${jobParams.branchName}; git pull; caasp-hyperv.ps1 deploy -caaspImage ${jobParams.image} -stackName ${jobParams.stackName} -adminRam ${jobParams.adminRam} -adminCpu ${jobParams.adminCpu} -masters ${jobParams.masterCount} -masterRam ${jobParams.masterRam} -masterCpu ${jobParams.masterCpu} -workers ${jobParams.workerCount} -workerRam ${jobParams.workerRam} -workerCpu ${jobParams.workerCpu} -Force' 2>&1 | tee ${WORKSPACE}/logs/caasp-hyperv.log")
         }
 
@@ -28,7 +28,7 @@ def createEnvironment(Map jobParams) {
 
 def destroyEnvironment(Map jobParams) {
     timeout(30) {
-        withEnv(["SSHUSER=${jobParams.credentials.hyperv_username}", "SSHPASS=${jobParams.credentials.hyperv_password}")]) {
+        withEnv(["SSHUSER=${jobParams.credentials.hyperv_username}", "SSHPASS=${jobParams.credentials.hyperv_password}"]) {
             sh(script: "set -o pipefail; sshpass -e ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \"${SSHUSER}\"@${jobParams.platformEndpoint} 'Get-ChildItem Env:; git checkout ${jobParams.branchName}; git pull; caasp-hyperv.ps1 destroy -caaspImage ${jobParams.image} -stackName ${jobParams.stackName} -masters ${jobParams.masterCount} -workers ${jobParams.workerCount} -Force' 2>&1 | tee ${WORKSPACE}/logs/caasp-hyperv.log")
         }
     }
